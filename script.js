7b971290c6ab5c3bc496801548a6d33c83f3e035
Jamie Foster <jamiefoster564@gmail.com>
2021-11-08 17:21:58 -0500

    added index, css and script code from sandbox

---
+++ script.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+let now = new Date();
+console.log(now.getDate());
+console.log(now.getSeconds());
+console.log(now.getDay());
+console.log(now.getFullYear());
+console.log(now.getMonth());
+
+let dayOfTheWeek = document.querySelector("#weekday");
+let dateTime = document.querySelector("#date-time");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+dayOfTheWeek.innerHTML = `${day}`;
+dateTime.innerHTML = `${month} ${date} ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#temperatureDegrees").innerHTML = `${temperature}°C`;
+}
+
+function search(event) {
+  let apiKey = "2b8193e1fb0166e722b7330ca6c589b4";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+let searchForm = document.querySelector("#city-search");
+searchForm.addEventListener("submit", handleSubmit);
